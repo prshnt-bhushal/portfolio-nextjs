4bc91e48493a31846415e3e802075744a72488de
Prshnt Bhushal <prshnt.bhushal@gmail.com>
2023-04-12 09:06:35 +0545

    Project page structured

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import ProjectsItems from './ProjectsItems'
+import Image from 'next/image'
+import tradeexImg from '../public/assets/projects/tradeexlogo.png'
+import routeImg from '../public/assets/projects/routelogo.png'
+import expensesImg from '../public/assets/projects/expenses.png'
+import Link from 'next/link'
 
 const Projects = () => {
   return (
-    <div></div>
+    <div className='w-full'>
+      <div className='max-w-[1240px] mx-auto px-2 py-16'>
+        <p className='text-xl tracking-widest uppercase'>Porjects</p>
+        <h2 className='py-4'>What I've Built</h2>
+        <div className='grid md:grid-cols-2 gap-8'>
+          <ProjectsItems title='Route' backgroundImg={routeImg} projectUrl='/property'/>
+          <ProjectsItems title='TradeEx' backgroundImg={tradeexImg} projectUrl='/property'/>
+          <ProjectsItems title='Expenses' backgroundImg={expensesImg} projectUrl='/property'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import Main from '@/components/Main'
 import About from '@/components/About'
 import Skills from '@/components/Skills'
+import Projects from '@/components/Projects'
 
 
 export default function Home() {
@@ -20,6 +21,7 @@ export default function Home() {
       <Main/>
       <About/>
       <Skills/>
+      <Projects/>
     </>
   )
 }
