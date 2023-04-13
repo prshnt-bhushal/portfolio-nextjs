c8f8699c55244c040a7f14796e1e5c999958056e
Prshnt Bhushal <prshnt.bhushal@gmail.com>
2023-04-13 07:41:32 +0545

    contact layout created

---
+++ components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='w-full'>
+      <div className='max-w-[1240px] mx-auto px-2 py-16'>
+        <p className='text-xl tracking-widest uppercase'>Contact</p>
+        <h2 className='py-4'>Find me Here</h2>
+      </div>
+    </div>)
+}
+
+export default Contact
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Main from '@/components/Main'
 import About from '@/components/About'
 import Skills from '@/components/Skills'
 import Projects from '@/components/Projects'
+import Contact from '@/components/Contact'
 
 
 export default function Home() {
@@ -22,6 +23,7 @@ export default function Home() {
       <About/>
       <Skills/>
       <Projects/>
+      <Contact/>
     </>
   )
 }
